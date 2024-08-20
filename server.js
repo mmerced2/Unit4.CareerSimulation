8a539f7ea6eacc2b91016f014c439c1af2bf841e
Maria Mercedes <mariamercedes@Marias-Air.lan>
2024-08-19 21:41:16 -0400

    "added cors"

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const server = express();
-
+const cors = require("cors");
 
 server.use(express.json());
+
+server.use(express.json());
+server.use(cors());
 
 server.get("/", (req,res) => {
     res.send({message: 'Working'});
